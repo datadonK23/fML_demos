1470da9ddbf9b4994c6c63db072568e00966b56c
datadonK23 <datadonk23@gmail.com>
2019-05-17 16:18:38 +0200

    make_dataset script. Loads and persist user complaints dataset for NLP classification demo.

---
+++ make_dataset.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+""" make_dataset
+
+Downloads datasets used in the demos and persist them in /data
+
+Author: datadonk23
+Date: 17.05.19 
+"""
+
+import pandas as pd
+import os, logging
+logging.getLogger().setLevel(logging.INFO)
+
+
+root_path = os.path.abspath(os.curdir)
+data_dir = os.path.join(root_path, "data")
+
+
+### NLPClassif_complaints
+# Consumer complaints dataset from Consumer Complaints Database, ~740MB
+
+url = "https://data.consumerfinance.gov/api/views/s6ew-h6mp/rows.csv?accessType=DOWNLOAD"
+file_name = "Consumer_Complaints.csv"
+file_path = os.path.join(data_dir, file_name)
+
+try:
+    logging.info("Start downloading complaints dataset")
+    consumer_complaints = pd.read_csv(url, dtype=object)
+    logging.info("Start persisting complaints dataset")
+    consumer_complaints.to_csv(file_path, index=False)
+except Exception as e:
+    logging.error("Unable to make nlp_classif_complaints dataset. "
+                  "Check for correct URL and path.",
+                  exc_info=e)
+
