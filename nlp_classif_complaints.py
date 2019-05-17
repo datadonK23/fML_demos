11a80f5f206ce1809f287187e80423f520116e08
datadonK23 <datadonk23@gmail.com>
2019-05-17 16:19:42 +0200

    WIP. nlp_classif_complaints structure.

---
+++ nlp_classif_complaints.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+""" nlp_classif_complaints
+
+Binary classification of user complaints. Predicts whether user asks about
+'Credit reporting, credit repair services, or other personal consumer reports'
+or not.
+
+Modified from NLP Classification demo of fklearn.
+(https://fklearn.readthedocs.io/en/latest/examples/nlp_classification.html)
+
+Author: datadonk23
+Date: 17.05.19 
+"""
+
+import numpy as np
+import pandas as pd
+
+
+# Load dataset
+#TODO
+
+# Split into train/dev
+#TODO
+
+# Train model
+#TODO
+
+# Predict
+#TODO
+
+# Evaluation
+#TODO
